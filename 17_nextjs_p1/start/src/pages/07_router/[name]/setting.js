0d0ef5a995cf7c5a48a954356bc83edb1e08f29b
rioto.nagano <kingdom950927@gmail.com>
2023-01-22 10:51:37 +0900

    ルートパスへ戻るボタンの作成

--- 17_nextjs_p1/start/src/pages/07_router/[name]/setting.js
+++ 17_nextjs_p1/start/src/pages/07_router/[name]/setting.js
@@ -2,8 +2,18 @@ import {useRouter} from 'next/router'
 
 export default function Setting({ query }) {
   const router = useRouter()
-  console.log(router);
-  return <h1>routerから取得:{ router. query.name}</h1>
+
+  // ボタンクリック時にルートパスへ戻る
+  const clickHandler = () => {
+    router.push('/')
+  }
+
+  return (
+    <>
+      <h1>routerから取得:{ router. query.name}</h1>
+      <button onClick={clickHandler}>アクションによる画面遷移</button>
+    </>
+  )
 }
 
 export async function getServerSideProps({ query }) {
