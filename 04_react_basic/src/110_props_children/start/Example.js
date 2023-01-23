60a22ba86fc615728f07b4065cfbbbdb6ee67243
rioto.nagano <kingdom950927@gmail.com>
2023-01-24 07:00:20 +0900

    childrenで子コンポーネントを親コンポーネントへ渡す

--- 04_react_basic/src/110_props_children/start/Example.js
+++ 04_react_basic/src/110_props_children/start/Example.js
@@ -1,15 +1,27 @@
 import Profile from "./components/Profile";
 import Container from "./components/Container";
+import WelcomeDialog from "./components/WelcomeDialog";
 
 const profile = [
-  { name: "Takashi", age: 19, country: "Japan", color: "green" },
-  { name: "Jane", age: 28, country: "UK", color: "blue" },
+  { name: "Takashi", age: 19, country: "Japan", color: "blue" },
+  { name: "Jane", age: 28, country: "UK", color: "red" },
 ];
+
+// スプレッド構文から0番目のオブジェクトを出力
+console.log({...profile[0]})
 
 const Example = () => {
   return (
     <div>
-      <Container title="Childrenとは？" />
+      <Container title="順番通り渡す" >
+        <Profile {...profile[0]} />
+        <Profile  {...profile[1]} />
+      </Container>
+      <Container title="個別に渡す" >
+        <Profile  {...profile[1]} />
+        <Profile {...profile[0]} />
+      </Container>
+      <WelcomeDialog />
     </div>
   );
 };
