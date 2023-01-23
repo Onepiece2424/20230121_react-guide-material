eb95585e6d13ea4fa4dc994d48fb21e8ec155043
rioto.nagano <kingdom950927@gmail.com>
2023-01-24 05:58:40 +0900

    exportする際の注意点

--- 18_nextjs_p2/start/src/pages/020_SG/index.js
+++ 18_nextjs_p2/start/src/pages/020_SG/index.js
@@ -1,3 +1,12 @@
-export default function IndexPage() {
-  return <h3>SG</h3>
+export default function IndexPage({ message }) {
+  return <h3>SG:{ message }</h3>
+}
+
+export async function getStaticProps() {
+
+  console.log('getStaticProps')
+  
+  return {
+    props: { message: ' From Static Props' }
+  }
 }
