257cbc85ee749a750f9c32574c9b01cecc1f804e
rioto.nagano <kingdom950927@gmail.com>
2023-01-27 06:51:46 +0900

    フォームに値を入力したときに入力値の表示

--- 05_state_and_event/src/020_useState/start/Example.js
+++ 05_state_and_event/src/020_useState/start/Example.js
@@ -1,8 +1,12 @@
+import { useState } from 'react'
+
 const Example = () => {
+
+  const [ text, setText] = useState("");
 
   return (
     <>
-      
+      <input onChange={(e) => setText(e.target.value)} />={text}
     </>
   );
 };
