a05ec44aabde29ecdd0c77e398a80bb2a867706c
rioto.nagano <kingdom950927@gmail.com>
2023-01-28 07:32:27 +0900

    カウントアップダウンボタンの作成

--- 05_state_and_event/src/050_prev_state/start/Example.js
+++ 05_state_and_event/src/050_prev_state/start/Example.js
@@ -1,7 +1,25 @@
 import { useState } from "react";
 
 const Example = () => {
-  
+
+  const[count,setCount] = useState(0)
+
+  const countUp = () => {
+    setCount(prev => prev + 2)
+  }
+
+  const countDown = () => {
+    setCount(prev => prev - 1)
+  }
+
+  return (
+    <>
+      <div>現在のカウント数: {count}</div>
+      <br></br>
+      <button onClick={countUp}>+</button>
+      <button onClick={countDown}>-</button>
+    </>
+  )
 };
 
 export default Example;
