55d2815f1c221ff877d56c104da84731cd7048d8
rioto.nagano <kingdom950927@gmail.com>
2023-01-28 12:18:15 +0900

    stateのオブジェクトを更新する別の方法

--- 05_state_and_event/src/060_state_object/start/Example.jsx
+++ 05_state_and_event/src/060_state_object/start/Example.jsx
@@ -1,32 +1,40 @@
 import { useState } from "react";
 
-const Example = () => {
-  const personObj = { name: "Tom", age: 18 };
+const Form = () => {
+  const personObj = { name: "John", age: 20, job: "student" };
 
   const [obj,setObj] = useState(personObj);
 
-  // 名前と年齢を入力するたびに変更する
+  // 名前、年齢、職業を入力するたびに変更する
   const changeName = (e) => {
-    setObj(prev => ({ ...prev, name: e.target.value }))
+    // setObj(prev => ({ ...prev, name: e.target.value }))
+    setObj(() => ({ name: e.target.value, age: obj.age, job: obj.job }))
   }
   const changeAge = (e) => {
-    setObj(prev => ({ ...prev, age: e.target.value }))
+    // setObj(prev => ({ ...prev, age: e.target.value }))
+    setObj(() => ({ name: obj.name, age: e.target.value, job: obj.job  }))
+  }
+  const changeJob = (e) => {
+    // setObj(() => ({ name: obj.name, age: obj.age, job: e.target.value }))
+    setObj(() => ({ name: obj.name, age: obj.age, job: e.target.value }))
   }
 
   // 入力フォームの値のリセット
   const valueReset = () => {
-    setObj({ name: "", age: "" })
+    setObj({ name: "", age: "", job: "" })
   }
 
   return (
     <>
       <h3>Name:{obj.name}</h3>
       <h3>Age:{obj.age}</h3>
+      <h3>Job:{obj.job}</h3>
       <input id="form1" onChange={changeName} value={obj.name} />
       <input id="form2" onChange={changeAge} value={obj.age} type='number' />
+      <input id="form3" onChange={changeJob} value={obj.job} />
       <div><button onClick={valueReset}>リセット</button></div>
     </>
   )
 };
 
-export default Example;
+export default Form;
