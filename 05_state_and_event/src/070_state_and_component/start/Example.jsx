55a958fa40a5ed6237b2f2f976319743741fe769
rioto.nagano <kingdom950927@gmail.com>
2023-01-28 16:13:52 +0900

    toggleボタンを作成し、ボタンを押下するごとに表示の切り替え

--- 05_state_and_event/src/070_state_and_component/start/Example.jsx
+++ 05_state_and_event/src/070_state_and_component/start/Example.jsx
@@ -2,15 +2,25 @@ import { useState } from "react";
 
 const Example = () => {
   const [count, setCount] = useState(0);
+  const [flag, setFlag] = useState(true)
+
+  // カウントアップダウン
   const countUp = () => {
     setCount((prevstate) => prevstate + 1);
   };
   const countDown = () => {
     setCount(count - 1);
   };
+
+  // toggleボタンの表示の切り替え
+  const changeFlag = () => {
+    setFlag(prev => !prev)
+  }
+
   return (
     <>
-      <h3>カウント: {count}</h3>
+      <button onClick={changeFlag}>toggle</button>
+      <h3>{flag ? "A" : "B"}: {count}</h3>
       <button onClick={countUp}>+</button>
       <button onClick={countDown}>-</button>
     </>
