2d0da5cdab40e60dfe0d5d76c8b6e089115839a2
rioto.nagano <kingdom950927@gmail.com>
2023-01-29 11:20:02 +0900

    配列をmapで回して表示

--- 06_control_and_form/src/010_list_components/start/Example.jsx
+++ 06_control_and_form/src/010_list_components/start/Example.jsx
@@ -1,13 +1,11 @@
 
-const animals = ["Dog", "Cat", "Rat"];
-
 const Example = () => {
-
+  const animals = ["Dog", "Cat", "Rat"];
   return (
     <>
       <h3>配列の操作</h3>
       <ul>
-        
+        {animals.map((animal) => <li key={animal.toString()}>Hello, {animal}</li>)}
       </ul>
     </>
   );
