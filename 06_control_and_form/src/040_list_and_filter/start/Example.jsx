671a62f5c7ceeaf165551dd6cb9372e79384a16e
rioto.nagano <kingdom950927@gmail.com>
2023-01-29 15:50:58 +0900

    filterメソッドを用いた検索フォームの作成

--- 06_control_and_form/src/040_list_and_filter/start/Example.jsx
+++ 06_control_and_form/src/040_list_and_filter/start/Example.jsx
@@ -3,13 +3,22 @@ import { useState } from "react";
 const animals = ["Dog", "Cat", "Rat"];
 
 const Example = () => {
+
+  const [value,setValue] = useState("")
+
+  const changeValue = (e) => {
+    setValue(e.target.value)
+  }
+
   return (
     <>
       <h3>配列のフィルター</h3>
+      <input onChange={changeValue} value={value} />
       <ul>
         {animals
+          .filter((animal) => animal.indexOf(value) !== -1)
           .map((animal) => (
-          <li>{animal}</li>
+          <li key={animal}>{animal}</li>
         ))}
       </ul>
     </>
