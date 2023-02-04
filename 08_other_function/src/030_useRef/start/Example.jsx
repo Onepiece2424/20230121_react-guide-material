bc4a065488589f19e3adf8164b7602bf1883c51f
rioto.nagano <kingdom950927@gmail.com>
2023-02-05 05:37:17 +0900

    動画再生プレイヤーの作成

--- 08_other_function/src/030_useRef/start/Example.jsx
+++ 08_other_function/src/030_useRef/start/Example.jsx
@@ -4,6 +4,17 @@ const Case1 = () => {
   const [value, setValue] = useState("");
   const inputRef = useRef()
 
+  const [videoState, setVideoState] = useState(false)
+  const videoRef = useRef();
+  const videoPlay = () => {
+    if (videoState) {
+      videoRef.current.pause()
+    } else {
+      videoRef.current.play()
+    }
+    setVideoState(!videoState)
+  }
+
   return (
     <div>
       <h3>ユースケース1</h3>
@@ -11,6 +22,12 @@ const Case1 = () => {
       <button onClick={() => inputRef.current.focus()}>
         インプット要素をフォーカスする
       </button>
+
+      <h3>ユースケース2</h3>
+      <video style={{ maxWidth: "100%" }} ref={videoRef}>
+        <source src="./sample.mp4"></source>
+      </video>
+      <button onClick={videoPlay}>{videoState ? "Stop" : "Play"}</button>
     </div>
   );
 };
