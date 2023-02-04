e11c8cfee3044e21275ba9c738a62de0235485ef
rioto.nagano <kingdom950927@gmail.com>
2023-02-05 07:19:51 +0900

    refオブジェクトを名前を変更し、関数コンポーネントへ渡す

--- 08_other_function/src/040_forwardRef/start/Example.jsx
+++ 08_other_function/src/040_forwardRef/start/Example.jsx
@@ -1,10 +1,14 @@
 import { useRef, forwardRef } from "react";
 
+const Input = ({ customRef }) => {
+  return <input type="text" ref={customRef}/>
+}
+
 const Example = () => {
   const ref = useRef();
   return (
     <>
-      <input type="text" ref={ref} />
+      <Input customRef={ref} />
       <button onClick={() => ref.current.focus()}>
         インプット要素をフォーカスする
       </button>
