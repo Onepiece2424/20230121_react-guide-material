03d95085bae6f5a22ef325e6c60b13a303b8d243
rioto.nagano <kingdom950927@gmail.com>
2023-02-05 08:10:42 +0900

    useImperativeHandleを使用し、refへのアクセスを限定

--- 08_other_function/src/040_forwardRef/start/Example.jsx
+++ 08_other_function/src/040_forwardRef/start/Example.jsx
@@ -1,7 +1,16 @@
-import { useRef, forwardRef } from "react";
+import { useRef, forwardRef, useImperativeHandle } from "react";
 
 const Input = forwardRef((props,  ref) => {
-  return <input type="text" ref={ref}/>
+
+  // Exampleコンポーネントから渡ってきたrefとは別のrefを定義する
+  const inputRef = useRef()
+
+  useImperativeHandle(ref, () => ({
+    myFocus() {
+      inputRef.current.focus()
+    }
+  }))
+  return <input type="text" ref={inputRef}/>
 })
 
 const Example = () => {
@@ -9,7 +18,7 @@ const Example = () => {
   return (
     <>
       <Input ref={ref} />
-      <button onClick={() => ref.current.focus()}>
+      <button onClick={() => ref.current.myFocus()}>
         インプット要素をフォーカスする
       </button>
     </>
