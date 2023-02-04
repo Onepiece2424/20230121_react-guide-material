f639f3eb220b8ee916d9ac0aa25c21c468889f85
rioto.nagano <kingdom950927@gmail.com>
2023-02-05 07:31:42 +0900

    forwardRefを使用し、refオブジェクトをInputコンポーネントへ渡す

--- 08_other_function/src/040_forwardRef/start/Example.jsx
+++ 08_other_function/src/040_forwardRef/start/Example.jsx
@@ -1,14 +1,14 @@
 import { useRef, forwardRef } from "react";
 
-const Input = ({ customRef }) => {
-  return <input type="text" ref={customRef}/>
-}
+const Input = forwardRef((props,  ref) => {
+  return <input type="text" ref={ref}/>
+})
 
 const Example = () => {
   const ref = useRef();
   return (
     <>
-      <Input customRef={ref} />
+      <Input ref={ref} />
       <button onClick={() => ref.current.focus()}>
         インプット要素をフォーカスする
       </button>
